d8fd8c82ad31b0dc38e63ecad5e4f6d18b10d031
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-14 22:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var recipes = {};
 // var recipes = new Object();
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(targetObject, updatesObject) {
+  return Object.assign(targetObject, updatesObject);
+}
+
+
+//function updateObjectWithKeyAndValue(object, key, value) {
 //  new Object({key: value});
 //  object = {key: value};
 //  object.key = value;
-Object.assign({key: value});
-return object;
-}
+//Object.assign({key: value});
+
+//}
 
 
 
 
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({key: value});
-}
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//  Object.assign({key: value});
+//}
