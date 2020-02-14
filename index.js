ed06c554e31ca5d738c8de39fb5efa6ab650bcda
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-14 22:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
 //  new Object({key: value});
-  object = {key: value};
+//  object = {key: value};
+  object[key] = value;
 }
