4e747d34f976ba193f67b883ed1e256d505b7162
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-14 23:01:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 
-//function updateObjectWithKeyAndValue(object, key, value) {
-//  new Object({key: value});
-//  object = {key: value};
-//  object.key = value;
-//Object.assign({key: value});
-
-//}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value});
+}
 
 
 
